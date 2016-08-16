2b9a274374e3a68dad4d7d028740c85254eb4f6f
peterly <peterly@hotmail.co.nz>
2016-08-16 17:49:52 +1200

    not

--- src/cluedo/Suggestion.java
+++ src/cluedo/Suggestion.java
@@ -1,29 +1,18 @@
+
 package cluedo;
 
 public class Suggestion {
-
-	String weapon;
-	String character;
-	String room;
-
-	public Suggestion(String room, String weapon, String character) {
+	
+	Weapon weapon;
+	Character character;
+	Room room;
+	
+	public Suggestion(Weapon weapon, Character character, Room room) {
 		this.weapon = weapon;
 		this.room = room;
 		this.character = character;
 	}
-
-	public boolean isValid() {
-		return (this.room != null && this.character != null && this.weapon != null);
-	}
-
-	public boolean equals(Suggestion s) {
-		if(s == null){
-			return false;
-		}	
-		return (this.weapon.equals(s.weapon) && this.character.equals(s.character) && this.room.equals(s.room));
-			
-	}
-
+	
 	public String toString() {
 		return String.format("W: %s C: %s R: %s", weapon.toString(), character.toString(), room.toString() );
 	}
